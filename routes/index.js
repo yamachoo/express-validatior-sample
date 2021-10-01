4e9bebda371cbfb3c8faef125a4b41a5ee1caaa1
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-10-01 22:52:40 +0900

    add セットアップ Example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const { body, validationResult } = require('express-validator')
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+const checkValidation = () => {
+  return (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    next()
+  }
+}
+
+router.post('/user', [
+  body('username').isEmail(),
+  body('password').isLength({ min: 5 }),
+  checkValidation()
+],
+(req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  }).then(user => res.json(user))
 })
 
 module.exports = router
